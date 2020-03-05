374e5ed7098e945964268bc7377d68604f0eed6e
Abdullah Fahhad <alking144@hotmail.com>
2020-03-05 23:40:28 +0300

    bottom nav bar

--- Waraqah/Waraqah/MainPage.xaml.cs
+++ Waraqah/Waraqah/MainPage.xaml.cs
@@ -5,13 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using BottomBar.XamarinForms;
+
 
 namespace Waraqah
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : BottomBarPage
     {
         public MainPage()
         {
